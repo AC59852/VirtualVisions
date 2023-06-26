d9c8d548b61fdb660224e17c1f0daffb2285eab6
AC59852 <austin.caron1@gmail.com>
2023-06-26 15:32:05 -0400

    added unoptimized feed data by pulling the users following list and getting those users posts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,54 @@
 <template>
   <RouterView />
 </template>
-<script>
+<script setup>
   import router from './router';
   import { getCurrentUser } from 'vuefire';
+  import { useUserStore } from '@/stores/user';
+  import { db } from '@/firebase';
+  import { doc, getDoc } from 'firebase/firestore';
 
   router.beforeEach(async (to) => {
-    if(to.meta.requiresAuth) {
-      const currentUser = await getCurrentUser();
+    const store = useUserStore();
 
-      if(!currentUser) {
+    if(to.meta.requiresAuth) {
+      // if the user in pinia already exists, don't fetch it again
+      if(store.currentUser !== null) {
+        console.log("user already exists")
+        return;
+      } else if (sessionStorage.getItem('user')) {
+        console.log("user in session storage")
+        store.currentUser = JSON.parse(sessionStorage.getItem('user'));
+        return;
+      } else {
+        console.log("building user")
+        const currentUser = await getCurrentUser();
+
+        const userRef = doc(db, 'users', currentUser.uid);
+        const userSnap = await getDoc(userRef);
+
+        if(userSnap.exists()) {
+          const user = userSnap.data();
+          store.currentUser = {
+            id: currentUser.uid,
+            email: user.email,
+            username: user.username,
+            name: user.firstName + " " + user.lastName,
+          };
+
+          // set session storage
+          sessionStorage.setItem('user', JSON.stringify(store.currentUser));
+        } else {
+          console.log('no such document');
+        }
+
+        if(!currentUser) {
         return {
           path: '/login',
           query: {
             redirect: to.fullPath
           }
+        }
         }
       }
     }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,30 +2,48 @@
   <main>
     <h1>Home</h1>
     <button @click.prevent="logout">Logout</button>
+    <router-link :to="`/profile/${store.currentUser.id}`">{{ store.currentUser.name }}</router-link>
+    <div v-for="post in posts" :key="post">
+      <img :src="post.file" alt="Image">
+    </div>
   </main>
 </template>
 <script>
-  import { collection } from 'firebase/firestore';
+  import { useFirebaseAuth, useFirebaseStorage } from 'vuefire'
+  import { doc, getDoc, query, where, collection, getDocs } from 'firebase/firestore';
+  import { useUserStore } from '@/stores/user';
   import { db } from '@/firebase';
-  import { useFirebaseAuth } from 'vuefire'
+  import { ref, getDownloadURL } from 'firebase/storage';
 
   export default {
     setup() {
       const auth = useFirebaseAuth();
+      const storage = useFirebaseStorage();
+
+      const store = useUserStore();
 
       return {
-        auth
+        auth,
+        store,
+        storage
       }
     },
 
     data() {
       return {
-        documents: []
+        posts: []
       }
+    },
+
+    mounted() {
+      this.getDocuments();
     },
 
     methods: {
       logout() {
+        // wipe the user
+        this.store.currentUser = null;
+
         this.auth.signOut()
           .then(() => {
             this.$router.push({ name: 'login' });
@@ -33,11 +51,60 @@
           .catch((error) => {
             console.log(error);
           });
-      }
-    },
+      },
 
-    firestore: {
-      documents: collection(db, 'documents')
+      async getDocuments() {
+        // using the currentUser's id, get the users they follow
+        const userRef = doc(db, 'users', this.store.currentUser.id);
+
+        await getDoc(userRef)
+          .then((doc) => {
+            if(doc.exists()) {
+              const user = doc.data();
+
+              // get the users the currentUser follows
+              const following = user.following;
+
+              // loop through the users the currentUser follows
+              for(let i = 0; i < following.length; i++) {
+                this.getSinglePost(following[i]);
+              }
+            } else {
+              console.log('no such document 1');
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+
+      async getSinglePost(follower) {
+      // Use the follower ID to query for their posts
+      const postQuery = query(collection(db, 'posts'), where('account', '==', follower));
+
+      const querySnapshot = await getDocs(postQuery);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+
+        const data = doc.data();
+
+        const postFileRef = ref(this.storage, data.path);
+
+        getDownloadURL(postFileRef)
+        .then((url) => {
+          const post = {
+          id: doc.id,
+          description: data.caption,
+          likes: data.likes,
+          file: url
+        }
+        
+        // push the post to the posts array
+        this.posts.push(post);
+        })
+      });
+    }
     }
   }
 </script>
