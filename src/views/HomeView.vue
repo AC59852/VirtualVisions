3d972dd53b509223cb2145ac94d8195494be87d2
AC59852 <austin.caron1@gmail.com>
2023-06-28 18:27:41 -0400

    added scripting for feed, limited api calls, created session storage for data on refresh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,102 +9,98 @@
   </main>
 </template>
 <script>
-  import { useFirebaseAuth, useFirebaseStorage } from 'vuefire'
-  import { doc, getDoc, query, where, collection, getDocs } from 'firebase/firestore';
-  import { useUserStore } from '@/stores/user';
-  import { db } from '@/firebase';
-  import { ref, getDownloadURL } from 'firebase/storage';
+import { useFirebaseAuth, useFirebaseStorage } from 'vuefire';
+import { doc, getDoc, query, where, collection, getDocs } from 'firebase/firestore';
+import { useUserStore } from '@/stores/user';
+import { db } from '@/firebase';
+import { ref, getDownloadURL } from 'firebase/storage';
 
-  export default {
-    setup() {
-      const auth = useFirebaseAuth();
-      const storage = useFirebaseStorage();
+export default {
+  setup() {
+    const auth = useFirebaseAuth();
+    const storage = useFirebaseStorage();
 
-      const store = useUserStore();
+    const store = useUserStore();
 
-      return {
-        auth,
-        store,
-        storage
-      }
-    },
+    return {
+      auth,
+      store,
+      storage
+    };
+  },
 
-    data() {
-      return {
-        posts: []
-      }
-    },
+  data() {
+    return {
+      posts: []
+    }
+  },
 
-    mounted() {
-      this.getDocuments();
-    },
+  async mounted() {
+    await this.getDocuments();
+  },
 
-    methods: {
-      logout() {
-        // wipe the user
-        this.store.currentUser = null;
+  methods: {
+    logout() {
+      this.store.currentUser = null;
 
-        this.auth.signOut()
-          .then(() => {
-            this.$router.push({ name: 'login' });
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-
-      async getDocuments() {
-        // using the currentUser's id, get the users they follow
-        const userRef = doc(db, 'users', this.store.currentUser.id);
-
-        await getDoc(userRef)
-          .then((doc) => {
-            if(doc.exists()) {
-              const user = doc.data();
-
-              // get the users the currentUser follows
-              const following = user.following;
-
-              // loop through the users the currentUser follows
-              for(let i = 0; i < following.length; i++) {
-                this.getSinglePost(following[i]);
-              }
-            } else {
-              console.log('no such document 1');
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-
-      async getSinglePost(follower) {
-      // Use the follower ID to query for their posts
-      const postQuery = query(collection(db, 'posts'), where('account', '==', follower));
-
-      const querySnapshot = await getDocs(postQuery);
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-
-        const data = doc.data();
-
-        const postFileRef = ref(this.storage, data.path);
-
-        getDownloadURL(postFileRef)
-        .then((url) => {
-          const post = {
-          id: doc.id,
-          description: data.caption,
-          likes: data.likes,
-          file: url
-        }
-        
-        // push the post to the posts array
-        this.posts.push(post);
+      this.auth
+        .signOut()
+        .then(() => {
+          this.$router.push({ name: 'login' });
         })
-      });
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    async getDocuments() {
+      // if posts are already stored in the session storage, use them
+      if (sessionStorage.getItem('posts')) {
+        this.posts = JSON.parse(sessionStorage.getItem('posts'));
+        console.log('posts already stored in the session storage')
+        return;
+      } else {
+
+      const userRef = doc(db, 'users', this.store.currentUser.id);
+      const userSnap = await getDoc(userRef);
+
+      if (userSnap.exists()) {
+        const user = userSnap.data();
+        const following = user.following;
+
+        const batchedQuery = query(
+          collection(db, 'posts'),
+          where('account', 'in', following)
+        );
+
+        const querySnapshot = await getDocs(batchedQuery);
+
+        for (const doc of querySnapshot.docs) {
+          const data = doc.data();
+          const postFileRef = ref(this.storage, data.path);
+
+          try {
+            const url = await getDownloadURL(postFileRef);
+            const post = {
+              id: doc.id,
+              description: data.caption,
+              likes: data.likes,
+              file: url
+            };
+
+            this.posts.push(post);
+
+            // store posts in the session storage
+            sessionStorage.setItem('posts', JSON.stringify(this.posts));
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      } else {
+        console.log('no such document');
+      }
     }
     }
   }
+};
 </script>
