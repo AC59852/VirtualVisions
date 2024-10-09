f25a4064d4d68e8a1ebb1292d0f669e59a6992e3
Anthony <alusco27@gmail.com>
2024-10-09 15:16:14 -0400

    Basic Google Login and Sign in functionality

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import {auth} from '$lib/firebase';
+    import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+    import { redirect } from '@sveltejs/kit';
+
+    async function loginWithGoogle() {
+        try{
+            const provider = new GoogleAuthProvider();
+
+            await signInWithPopup(auth,provider);
+
+            throw redirect(308, '/signedin');
+        } catch (e){
+            console.log(e);
+        }
+    }
+
+
+</script>
+
+
+<h1>Log in</h1>
+<a href="/signedin"><button on:click={loginWithGoogle}>Log in with Google</button></a>
+
+<p>No account? <a href="/signup">Sign Up!</a></p>
---
+++ src/routes/signedin/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {auth} from '$lib/firebase';
+
+    const user = auth.currentUser;
+</script>
+
+
+
+<h1>Signed In</h1>
+
+<p>Welcome {user.uid}</p>
